4892ebc595e6737c9623703535fe9b0373dfe358
unknown <frfontoura@gmail.com>
2018-03-06 20:58:34 -0300

    first commit

---
+++ exercicios_webpack/ex/index.js
@@ -0,0 +1,7 @@
+import Pessoa from './pessoa'
+
+const pessoa = new Pessoa('Fernando')
+console.log(pessoa.toString())
+
+//const logger = require('./logger')
+//logger.info('Usando o padrão CommonJs!')
