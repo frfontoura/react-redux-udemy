a9d192bcc8d659269bf1c9a96219bff6ea30bd17
unknown <frfontoura@gmail.com>
2018-03-07 20:41:19 -0300

    Passando propriedades para atributos filhos

--- exercicios_react/ex/family.jsx
+++ exercicios_react/ex/family.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Member from './member'
+import {childrenWithProps} from '../utils/reactUtils'
 
 export default props => (
     <div>
-        <Member name="Teste1" lastName="Anfren1" />
-        <Member name="Teste2" lastName="Anfren2" />
-        <Member name="Teste3" lastName="Anfren3" />
-        <Member name="Teste4" lastName="Anfren4" />
+        <h1>Familia</h1>
+        { childrenWithProps(props.children, props) }
     </div>
 )
--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Family from './family'
+import Member from './member'
 
-ReactDOM.render(<Family />, document.getElementById('app'))
+ReactDOM.render(
+    <Family lastName="Asdf">
+        <Member name="Teste1" />
+        <Member name="Teste2" />
+        <Member name="Teste3" />
+        <Member name="Teste4" />
+    </Family>, 
+    document.getElementById('app'))
