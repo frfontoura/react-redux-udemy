57df348bf81d5cad07587954f7722fbfe7e8869e
unknown <frfontoura@gmail.com>
2018-03-08 20:09:31 -0300

    Componente com estado (contador)

--- exercicios_react/ex.6/index.jsx
+++ exercicios_react/ex.6/index.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ClassComponet from './classComponent'
 
-ReactDOM.render(<ClassComponet label = 'Contador' initialValue={10} />, 
+ReactDOM.render(<ClassComponet value="Componente Classe" />, 
     document.getElementById('app'))
