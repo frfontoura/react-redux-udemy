3fd44c6c2a59d596bb055cf933af200b7620625e
unknown <frfontoura@gmail.com>
2018-03-19 11:18:41 -0300

    Evento description onChange

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -8,18 +8,28 @@ export default class Todo extends Component {
 
     constructor(props) {
         super(props)
+        this.state = { description: '', list: []}
+
+        this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
     }
 
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value})
+    }
+
     handleAdd() {
-        console.log(this)
+        console.log(this.state.description)
     }
 
     render() {
         return (
             <div>
                 <Header name='Todo' small='Cadastro' />
-                <TodoForm handleAdd={this.handleAdd} />
+                <TodoForm 
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange}
+                    description={this.state.description} />
                 <TodoList />
             </div>
         )
