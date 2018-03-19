6814fd3951943d78d99ca129d68b1d2a5470ff5b
unknown <frfontoura@gmail.com>
2018-03-19 10:51:17 -0300

    Estrutura botao add

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -5,11 +5,21 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd() {
+        console.log(this)
+    }
+
     render() {
         return (
             <div>
                 <Header name='Todo' small='Cadastro' />
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
