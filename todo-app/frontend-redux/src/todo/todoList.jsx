480f42380116b53716663c80e75e038d658cb473
unknown <frfontoura@gmail.com>
2018-03-22 21:04:03 -0300

    Configuracao Redux TodoApp

---
+++ todo-app/frontend-redux/src/todo/todoList.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import IconButton from '../template/iconButton'
+
+const Todo = props => {
+    
+    const renderRows = () => {
+        const list = props.list || []
+
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td className={todo.done ? 'markedAsDone': ''}>{todo.description}</td>
+                <td>
+                    <IconButton style='success' icon='check' onClick={() => props.handleMarkAsDone(todo)} hide={todo.done}></IconButton>
+                    <IconButton style='warning' icon='refresh' onClick={() => props.handleMarkAsPending(todo)} hide={!todo.done}></IconButton>
+                    <IconButton style='danger' icon='trash-o' onClick={() => props.handleRemove(todo)}></IconButton>
+                </td>
+            </tr>
+        ))
+    }
+
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th className='tableActions'>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
+
+const mapStateToProps = state => ({list: state.todo.list })
+export default connect(mapStateToProps)(Todo)
