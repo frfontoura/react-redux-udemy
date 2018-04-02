224af28aa39edc63b1e56503eef2a42b51cd9da2
unknown <frfontoura@gmail.com>
2018-04-02 20:22:19 -0300

    Dashboard sem redux

--- my-money-app/frontend/src/main/routes.jsx
+++ my-money-app/frontend/src/main/routes.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 
 import Dashboard from '../dashboard/dashboard'
+import Dashboard2 from '../dashboard2/dashboard2'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
     <Router history={hashHistory}>
         <Route path='/' component={Dashboard} />
         <Route path='/billingCycles' component={BillingCycle} />
+        <Route path='/dashboard2' component={Dashboard2} />
         <Redirect from='*' to='/' />
     </Router>
 )
