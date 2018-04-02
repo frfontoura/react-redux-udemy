20a777dd591dc406a9c47ea13adf9edcbc473046
unknown <frfontoura@gmail.com>
2018-04-02 20:58:22 -0300

    Componente Tab

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -3,6 +3,11 @@ import React, { Component } from 'react'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabsContent from '../common/tab/tabsContent'
+import TabHeader from '../common/tab/tabHeader'
+
 class BillingCycle extends Component {
 
     render() {
@@ -10,7 +15,17 @@ class BillingCycle extends Component {
             <div>
                 <ContentHeader title='Ciclos de Pagamento' small='Cadastro' />
                 <Content>
-                    Cadastro de ciclos de pagamentos
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList' />
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+                        </TabsHeader>
+                        <TabsContent>
+                            
+                        </TabsContent>
+                    </Tabs>
                 </Content>
             </div>
         )
