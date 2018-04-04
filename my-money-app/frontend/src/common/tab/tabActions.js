c1a175f6999bea52b6cf2b650a49ca78918ca0ca
unknown <frfontoura@gmail.com>
2018-04-03 21:02:20 -0300

    Componente de tabs

---
+++ my-money-app/frontend/src/common/tab/tabActions.js
@@ -0,0 +1,15 @@
+export function selectTab(tabId) {
+    return {
+        type: 'TAB_SELECTED',
+        payload: tabId
+    }
+}
+
+export function showTabs(...tabIds) {
+    const tabsToShow = {}
+    tabIds.forEach(tab => tabsToShow[tab] = true)
+    return {
+        type: 'TAB_SHOWED',
+        payload: tabsToShow
+    }
+}
