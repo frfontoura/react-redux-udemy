7a1bcd8f97c89acc0b6a22fd3e9cee2e65780cc9
unknown <frfontoura@gmail.com>
2018-04-05 20:50:58 -0300

    Componente BillingCycleList

--- my-money-app/frontend/src/common/template/footer.jsx
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -4,7 +4,7 @@ export default props => (
     <footer className='main-footer'>
         <strong>
             Copyright &copy; 2018
-            <a href='https://github.com/frfontoura' target='_blank'> @frfontoura </a>.
+            <a href='https://github.com/frfontoura' target='_blank'> @frfontoura </a>
         </strong>
     </footer>
 )
