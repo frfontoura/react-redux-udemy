966551c0932820f580d72e46e59755060dd2a113
unknown <frfontoura@gmail.com>
2018-04-07 12:02:37 -0300

    Criacao do BillingCycleForm

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -13,6 +13,8 @@ import TabContent from '../common/tab/tabContent'
 import { selectTab, showTabs } from '../common/tab/tabActions'
 
 import List from './billingCycleList'
+import Form from './billingCycleForm'
+import { create } from './billingCycleActions'
 
 class BillingCycle extends Component {
 
@@ -34,8 +36,12 @@ class BillingCycle extends Component {
                             <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
                         </TabsHeader>
                         <TabsContent>
-                            <TabContent id='tabList'><List /></TabContent>
-                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabList'>
+                                <List />
+                            </TabContent>
+                            <TabContent id='tabCreate'>
+                                <Form onSubmit={this.props.create}/>
+                            </TabContent>
                             <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
                             <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
@@ -47,5 +53,5 @@ class BillingCycle extends Component {
 
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs, create}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
--- my-money-app/frontend/src/billingCycle/billingCycleActions.js
+++ my-money-app/frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3003/api'
 
@@ -7,5 +10,22 @@ export function getList() {
     return {
         type: 'BILLING_CLYCLES_FETCHED',
         payload: request
+    }
+}
+
+export function create(values) {
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação realizada com sucesso.')
+                dispatch([
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList','tabCreate')
+                ])
+            }).catch (e => {
+                e.response.data.errors.forEach(error => { toastr.error('Erro', error) });
+            })
     }
 }
